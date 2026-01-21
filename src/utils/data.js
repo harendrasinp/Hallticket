958cc5ecec8758ed0c87b1dd1720e31d14752f6f
unknown <haren111990@gmail.com>
2026-01-21 17:38:23 +0530

    update project

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,7 @@
 const InfoData={
     "English":"  1.In case of any issue while generating the hall ticket, please contact the provided helpline number for assistance.9867775626",
 
-    "Gujarati":"2.હોલ ટિકિટ જનરેટ કરતી વખતે કોઈપણ પ્રકારની મુશ્કેલી અથવા ત્રુટિ થાય તો કૃપા કરીને આપેલ હેલ્પલાઇન નંબર પર સંપર્ક કરી સહાય પ્રાપ્ત કરો.હેલ્પલાઇન નંબર. 9867775626",
+    "Gujarati":"2.હોલ ટિકિટ જનરેટ કરતી વખતે કોઈપણ પ્રકારની મુશ્કેલી અથવા ત્રુટિ થાય તો કૃપા કરીને આપેલ હેલ્પલાઇન નંબર પર સંપર્ક કરી સહાય પ્રાપ્ત કરો.હેલ્પલાઇન નંબર.9867775626",
 }
 
 export default InfoData;
