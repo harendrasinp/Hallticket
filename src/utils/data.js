65368c1a2babd2d50fe76c80c2be56fee7d9c7f0
unknown <haren111990@gmail.com>
2026-01-21 17:05:15 +0530

    help line phone number change

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,7 @@
 const InfoData={
-    "English":"  1.In case of any issue while generating the hall ticket, please contact the provided helpline number for assistance.9638611000",
+    "English":"  1.In case of any issue while generating the hall ticket, please contact the provided helpline number for assistance.9867775626",
 
-    "Gujarati":"2.હોલ ટિકિટ જનરેટ કરતી વખતે કોઈપણ પ્રકારની મુશ્કેલી અથવા ત્રુટિ થાય તો કૃપા કરીને આપેલ હેલ્પલાઇન નંબર પર સંપર્ક કરી સહાય પ્રાપ્ત કરો.હેલ્પલાઇન નંબર. 9638611000",
+    "Gujarati":"2.હોલ ટિકિટ જનરેટ કરતી વખતે કોઈપણ પ્રકારની મુશ્કેલી અથવા ત્રુટિ થાય તો કૃપા કરીને આપેલ હેલ્પલાઇન નંબર પર સંપર્ક કરી સહાય પ્રાપ્ત કરો.હેલ્પલાઇન નંબર.9867775626",
 
     "Ticket":"3.જો સંપૂર્ણ નામ દાખલ કરવા છતાં હોલ ટિકિટ જનરેટ ન થાય, તો કૃપા કરીને નીચે આપેલ વિકલ્પોમાંથી કોઈ એક રીતથી નામ દાખલ કરીને ફરી પ્રયાસ કરો:",
 
