93201545062aa891a2215b77716e7c07e56b2f6a
unknown <haren111990@gmail.com>
2026-01-21 17:43:50 +0530

    update project

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import axios from "axios";
-import InfoData from "../utils/data";
+import InfoData from "@/utils/data";
 
 export default function Home() {
   const [mobile, setMobile] = useState("");
